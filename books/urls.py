ede4b3c0c77e86c7e4a0ad9df475098891963ea2
federico.spatola <federico.spatola@immobiliare.it>
2025-04-15 00:39:35 +0200

    feat: add views to add a book or to return the list of books

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('books/table/', views.books_table, name='books_table'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/', views.books, name='books'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponseNotAllowed
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Libro
@@ -21,3 +22,27 @@ def books_table(request):
     # Passa la pagina dei libri al template
     return render(request, 'books/table.html', {'page_obj': page_obj})
 
+# Aggiungi un libro
+def add_book(request):
+    if request.method == 'POST':
+        titolo = request.POST.get('titolo')
+        autore = request.POST.get('autore')
+        editore = request.POST.get('editore')
+        anno_edizione = request.POST.get('anno_edizione')
+
+        if titolo and autore and editore and anno_edizione:
+            Libro.objects.create(
+                titolo=titolo,
+                autore=autore,
+                editore=editore,
+                anno_edizione=anno_edizione
+            )
+            return HttpResponse('Libro aggiunto con successo!')  # Successo
+        else:
+            return HttpResponse('Dati incompleti. Per favore, completa tutti i campi.', status=400)  # Errore
+    return HttpResponseNotAllowed(['POST'])
+
+# Vista per ottenere tutti i libri in formato JSON
+def books(request):
+    books_list = Libro.objects.all().values('id', 'titolo', 'autore', 'editore', 'anno_edizione').order_by('anno_edizione')
+    return JsonResponse(list(books_list), safe=False)
