cfc71666fcccef8aef5e7dba29551ec256431c7a
federico.spatola <federico.spatola@immobiliare.it>
2025-04-14 23:49:01 +0200

    fix: edit view

--- books/views.py
+++ books/views.py
@@ -7,7 +7,19 @@ def home(request):
 def list_books(request):
     books = Libro.objects.all().order_by('anno_edizione')
     books_data = [
-        {'titolo': book.titolo, 'anno_edizione': book.anno_edizione}
+        {
+            'titolo': book.titolo,
+            'anno_edizione': book.anno_edizione,
+            'editore': {
+                'ragione_sociale': book.editore.ragione_sociale,
+                'indirizzo': book.editore.indirizzo,
+                'telefono': book.editore.telefono,
+            },
+            'autore': {
+                'nome': book.autore.nome,
+                'cognome': book.autore.cognome,
+            }
+        }
         for book in books
     ]
     return JsonResponse({'books': books_data})
