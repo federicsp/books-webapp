f9f5892ad367d3762dc83a7b3b03fa93a02b24fb
federico.spatola <federico.spatola@immobiliare.it>
2025-05-11 16:12:35 +0200

    refactored importer.py to encapsulate the data import logic in a function

--- scripts/importer.py
+++ scripts/importer.py
@@ -6,22 +6,23 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "books_webapp.settings")
 django.setup()
 
-with open("scripts/db.json") as f:
-    data = json.load(f)
+def run():
+    with open("scripts/db.json") as f:
+        data = json.load(f)
 
-for e in data["editori"]:
-    e = {k.replace(" ", "_"): v for k, v in e.items()}
-    Editore.objects.update_or_create(id=e["id"], defaults=e)
+    for e in data["editori"]:
+        e = {k.replace(" ", "_"): v for k, v in e.items()}
+        Editore.objects.update_or_create(id=e["id"], defaults=e)
 
-for a in data["autori"]:
-    a = {k.replace(" ", "_"): v for k, v in a.items()}
-    Autore.objects.update_or_create(id=a["id"], defaults=a)
+    for a in data["autori"]:
+        a = {k.replace(" ", "_"): v for k, v in a.items()}
+        Autore.objects.update_or_create(id=a["id"], defaults=a)
 
-for lib in data["libri"]:
-    lib = {k.replace(" ", "_"): v for k, v in lib.items()}
-    Libro.objects.update_or_create(
-        titolo=lib["titolo"],
-        autore_id=lib["autore"],
-        editore_id=lib["editore"],
-        anno_edizione=lib["anno_edizione"],
-    )
+    for lib in data["libri"]:
+        lib = {k.replace(" ", "_"): v for k, v in lib.items()}
+        Libro.objects.update_or_create(
+            titolo=lib["titolo"],
+            autore_id=lib["autore"],
+            editore_id=lib["editore"],
+            anno_edizione=lib["anno_edizione"],
+        )
